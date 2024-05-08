af34552ea98620593128059fa694a63861d4ffac
Claire Orsagos <155546312+orsagos1@users.noreply.github.com>
2024-05-08 07:16:27 -0400

    Update knexfile.js
    Updated DATABASE_URL

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,16 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://dev_pcn2_user:BuO3GE6DPP9smqlJm1WprPhvSmxRQyFT@dpg-cotljvv109ks73anbcmg-a.ohio-postgres.render.com/dev_pcn2",
 } = process.env;
 
 module.exports = {
   development: {
-    client: "postgresql",
-    connection: DATABASE_URL,
+    client: "pg",
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
