f1d3cc137d850b7e88e3f3e4f7cd54727c4cec00
Claire Orsagos <155546312+orsagos1@users.noreply.github.com>
2024-05-08 07:25:05 -0400

    Updated the production connection to include SSL

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,10 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
